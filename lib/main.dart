087fcaf4924f8bc44b547f4fa9ddb80738cb4d0d
NilaAyu1611 <nilaayucw@gmail.com>
2025-03-13 20:43:23 +0700

    mengatur top appbar

--- lib/main.dart
+++ lib/main.dart
@@ -17,3 +17,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyWeatherScreen extends StatelessWidget {
+  const MyWeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 103, 145, 6),
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.black),
+          onPressed: () {},
+        ),
+        title: const Text(
+          'Weather App',
+          style: TextStyle(color: Colors.black, fontSize: 18),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add_box_outlined, color: Color.fromARGB(255, 0, 0, 0)),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+      
+}
+}
+
